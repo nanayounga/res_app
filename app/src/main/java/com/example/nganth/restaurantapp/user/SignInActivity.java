6740353a9dbd0e7d03f7c6a6b7f8ede25e2243ca
EVA-DungLT <dung@remcorp.jp>
2018-06-04 17:03:39 +0700

    add sign in function

--- app/src/main/java/com/example/nganth/restaurantapp/user/SignInActivity.java
+++ app/src/main/java/com/example/nganth/restaurantapp/user/SignInActivity.java
@@ -1,58 +1,205 @@
 package com.example.nganth.restaurantapp.user;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignInActivity extends AppCompatActivity {
+import com.example.nganth.restaurantapp.BaseActivity;
+import com.example.nganth.restaurantapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-    private com.example.nganth.restaurantapp.databinding.SignInBinding binding;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = android.databinding.DataBindingUtil.setContentView(this, com.example.nganth.restaurantapp.R.layout.sign_in);
+public class SignInActivity extends BaseActivity implements
+            View.OnClickListener {
 
-        android.content.Intent intent = getIntent();
+        private static final String TAG = "EmailPassword";
+
+        private TextView mStatusTextView;
+        private TextView mDetailTextView;
+        private EditText mEmailField;
+        private EditText mPasswordField;
+
+        // [START declare_auth]
+        private FirebaseAuth mAuth;
+        // [END declare_auth]
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.sign_in);
+
+            // Views
+            mEmailField = findViewById(R.id.email_field);
+            mPasswordField = findViewById(R.id.password_field);
+            mDetailTextView = findViewById(R.id.detail);
+
+            // Buttons
+            findViewById(R.id.btnSignIn).setOnClickListener(this);
+            findViewById(R.id.verify_email_button).setOnClickListener(this);
+            findViewById(R.id.btnSignOut).setOnClickListener(this);
+            findViewById(R.id.signup_link).setOnClickListener(this);
+            // [START initialize_auth]
+            mAuth = FirebaseAuth.getInstance();
+            // [END initialize_auth]
+        }
+
+        // [START on_start_check_user]
+        @Override
+        public void onStart() {
+            super.onStart();
+            // Check if user is signed in (non-null) and update UI accordingly.
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            updateUI(currentUser);
+        }
+        // [END on_start_check_user]
+
+        private void signIn(String email, String password) {
+            Log.d(TAG, "signIn:" + email);
+            if (!validateForm()) {
+                return;
+            }
+
+            showProgressDialog();
+
+            // [START sign_in_with_email]
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(com.example.nganth.restaurantapp.user.SignInActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+
+                            // [START_EXCLUDE]
+
+                            if (!task.isSuccessful()) {
+                                mDetailTextView.setText(R.string.auth_failed);
+                            }
+                            hideProgressDialog();
+                            // [END_EXCLUDE]
+                        }
+                    });
+            // [END sign_in_with_email]
+        }
+
+        private void signOut() {
+            mAuth.signOut();
+            updateUI(null);
+        }
+
+    private void sendEmailVerification() {
+        // Disable button
+        findViewById(R.id.verify_email_button).setEnabled(false);
+
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // [START_EXCLUDE]
+                        // Re-enable button
+                        findViewById(R.id.verify_email_button).setEnabled(true);
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignInActivity.this,
+                                    "Verification email sent to " + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(SignInActivity.this,
+                                    "Failed to send verification email.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END send_email_verification]
     }
+        private boolean validateForm() {
+            boolean valid = true;
 
-    public void openSignUpActivity(android.view.View view) {
-        android.content.Intent intent = new android.content.Intent(getApplicationContext(), SignUpActivity.class);
-        startActivityForResult(intent, 1);// co ket qua tra ve
-    }
+            String email = mEmailField.getText().toString();
+            if (TextUtils.isEmpty(email)) {
+                mEmailField.setError("Required.");
+                valid = false;
+            } else {
+                mEmailField.setError(null);
+            }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+            String password = mPasswordField.getText().toString();
+            if (TextUtils.isEmpty(password)) {
+                mPasswordField.setError("Required.");
+                valid = false;
+            }else {
+                mPasswordField.setError(null);
+            }
 
-        if (resultCode == RESULT_OK) {
-            if (requestCode == 1) {
-                String beginPhone = data.getStringExtra("beginPhone");
-                String Phone = data.getStringExtra("Phone");
-                String Password = data.getStringExtra("Password");
+            return valid;
+        }
 
-                binding.txtBeginPhoneSignIn.setText(beginPhone);
-                binding.txtPhoneSignIn.setText(Phone);
-                binding.txtPasswordSignIn.setText(Password);
+        private void updateUI(FirebaseUser user) {
+            hideProgressDialog();
+           if (user != null) {
+                String user_detail = getString(R.string.emailpassword_status_fmt,
+                        user.getEmail(), user.isEmailVerified()) + "***" +getString(R.string.firebase_status_fmt, user.getUid());
+                mDetailTextView.setText(user_detail);
+
+                findViewById(R.id.btnSignIn).setVisibility(View.INVISIBLE);
+                findViewById(R.id.btnSignOut).setVisibility(View.VISIBLE);
+                if(user.isEmailVerified()){
+                    findViewById(R.id.verify_email_button).setVisibility(View.INVISIBLE);
+                }else{
+                    findViewById(R.id.verify_email_button).setVisibility(View.VISIBLE);
+                }
+            } else {
+                mDetailTextView.setText(null);
+
+               findViewById(R.id.btnSignIn).setVisibility(View.VISIBLE);
+               findViewById(R.id.btnSignOut).setVisibility(View.INVISIBLE);
+               findViewById(R.id.verify_email_button).setVisibility(View.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            int i = v.getId();
+            if (i == R.id.btnSignIn) {
+                signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+            }
+            else if (i == R.id.signup_link) {
+                //open activity signup
+                android.content.Intent intent = new android.content.Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+            else if (i == R.id.verify_email_button) {
+                sendEmailVerification();
+            }
+            else if (i == R.id.btnSignOut) {
+                signOut();
             }
         }
     }
-
-    public void openVerificationActivity(View view) {
-        android.content.Intent intent = new android.content.Intent(getApplicationContext(), VerificationActivity.class);
-
-        String valueBeginPhoneSignIn = binding.txtBeginPhoneSignIn.getText().toString();
-        intent.putExtra("valueBeginPhoneSignIn", valueBeginPhoneSignIn);
-
-        String valuePhoneSignIn = binding.txtPhoneSignIn.getText().toString();
-        intent.putExtra("valuePhoneSignIn", valuePhoneSignIn);
-
-        String valuePasswordSignIn = binding.txtPasswordSignIn.getText().toString();
-        intent.putExtra("valuePasswordSignIn", valuePasswordSignIn);
-
-        startActivity(intent);
-    }
-}
--- app/src/main/java/com/example/nganth/restaurantapp/user/SignUpActivity.java
+++ app/src/main/java/com/example/nganth/restaurantapp/user/SignUpActivity.java
@@ -1,50 +1,175 @@
 package com.example.nganth.restaurantapp.user;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.example.nganth.restaurantapp.BaseActivity;
+import com.example.nganth.restaurantapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+public class SignUpActivity extends BaseActivity implements
+        View.OnClickListener {
 
-    private com.example.nganth.restaurantapp.databinding.SignUpBinding binding;
-//    @Override
+        private static final String TAG = "EmailPassword";
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = android.databinding.DataBindingUtil.setContentView(this, com.example.nganth.restaurantapp.R.layout.sign_up);
+        private EditText mEmailField;
+        private EditText mPasswordField;
+        private EditText mConfirmPasswordField;
 
-        final Intent intent = getIntent();
+        // [START declare_auth]
+        private FirebaseAuth mAuth;
+        // [END declare_auth]
 
-        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //lay gia tri nhap
-                String valueBeginPhone = binding.txtBeginPhone.getText().toString();
-                intent.putExtra("beginPhone", valueBeginPhone);
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.sign_up);
 
-                String valuePhone = binding.txtPhone.getText().toString();
-                intent.putExtra("Phone", valuePhone);
+            // Views
 
-                String valuePassword = binding.txtPassword.getText().toString();
-                intent.putExtra("Password", valuePassword);
+            mEmailField = findViewById(R.id.email_field);
+            mPasswordField = findViewById(R.id.password_field);
+            mConfirmPasswordField = findViewById(R.id.confirm_password_field);
 
-                String valueConfirmPassword = binding.txtConfirmPassword.getText().toString();
-                intent.putExtra("ConfirmPassword", valueConfirmPassword);
+            // Buttons
+            findViewById(R.id.btnSignUp).setOnClickListener(this);
+            findViewById(R.id.signin_link).setOnClickListener(this);
 
-                setResult(RESULT_OK, intent);
-                finish();
+            // [START initialize_auth]
+            mAuth = FirebaseAuth.getInstance();
+            // [END initialize_auth]
+        }
+
+        // [START on_start_check_user]
+        @Override
+        public void onStart() {
+            super.onStart();
+            // Check if user is signed in (non-null) and update UI accordingly.
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            //updateUI(currentUser);
+        }
+        // [END on_start_check_user]
+
+        private void createAccount(String email, String password) {
+            Log.d(TAG, "createAccount:" + email);
+            if (!validateForm()) {
+                return;
             }
-        });
-    }
 
-    public void openVerificationActivity(android.view.View view) {
-        android.content.Intent intent = new android.content.Intent(getApplicationContext(), VerificationActivity.class);
-        startActivity(intent);
-    }
+            showProgressDialog();
 
-    public void openSignInActivity(android.view.View view) {
-        android.content.Intent intent = new android.content.Intent(getApplicationContext(), SignInActivity.class);
-        startActivity(intent);
+            // [START create_user_with_email]
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                //Send email verification
+                                sendEmailVerification();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                            // [START_EXCLUDE]
+                            hideProgressDialog();
+                            // [END_EXCLUDE]
+                        }
+                    });
+            // [END create_user_with_email]
+        }
+
+        private void sendEmailVerification() {
+            // Disable button
+            findViewById(R.id.verify_email_button).setEnabled(false);
+
+            // Send verification email
+            // [START send_email_verification]
+            final FirebaseUser user = mAuth.getCurrentUser();
+            user.sendEmailVerification()
+                    .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            // [START_EXCLUDE]
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignUpActivity.this,
+                                        "Verification email sent to " + user.getEmail(),
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                Log.e(TAG, "sendEmailVerification", task.getException());
+                                Toast.makeText(SignUpActivity.this,
+                                        "Failed to send verification email.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                            // [END_EXCLUDE]
+                        }
+                    });
+            // [END send_email_verification]
+        }
+
+        private boolean validateForm() {
+            boolean valid = true;
+
+            String email = mEmailField.getText().toString();
+            if (TextUtils.isEmpty(email)) {
+                mEmailField.setError("Required.");
+                valid = false;
+            } else {
+                mEmailField.setError(null);
+            }
+
+            String password = mPasswordField.getText().toString();
+            String confirm_password = mConfirmPasswordField.getText().toString();
+            if (TextUtils.isEmpty(password)) {
+                mPasswordField.setError("Required.");
+                valid = false;
+            }else if( ! password.equals(confirm_password)){
+                mConfirmPasswordField.setError("Confirm password not match.");
+                valid = false;
+            }else {
+                mPasswordField.setError(null);
+            }
+
+            return valid;
+        }
+
+       /* private void updateUI(FirebaseUser user) {
+            hideProgressDialog();
+            if (user != null) {
+                mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
+                        user.getEmail(), user.isEmailVerified()));
+                mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+            } else {
+                mStatusTextView.setText(R.string.signed_out);
+                mDetailTextView.setText(null);
+            }
+        }*/
+
+        @Override
+        public void onClick(View v) {
+            int i = v.getId();
+            if (i == R.id.btnSignUp) {
+                createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
+            }
+            else if (i == R.id.signin_link) {
+                android.content.Intent intent = new android.content.Intent(getApplicationContext(), SignInActivity.class);
+                startActivity(intent);
+            }
+        }
     }
-}
