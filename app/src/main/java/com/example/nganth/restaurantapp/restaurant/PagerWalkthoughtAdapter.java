87cbc4b1bb9d0ccc316c685f9536a5ba0fc20886
nanayounga <nganth@evolableasia.vn>
2018-06-21 08:45:07 +0700

    finish_walkthought

--- app/src/main/java/com/example/nganth/restaurantapp/restaurant/PagerWalkthoughtAdapter.java
+++ app/src/main/java/com/example/nganth/restaurantapp/restaurant/PagerWalkthoughtAdapter.java
@@ -16,9 +16,6 @@ public class PagerWalkthoughtAdapter extends FragmentStatePagerAdapter {
     public PagerWalkthoughtAdapter(FragmentManager fm) {
         super(fm);
 
-//        https://maps.googleapis.com/maps/api/place/search/json?sensor=false&key=AIzaSyB5EOs3RT36APJACuSjayKvJmUTTO9_1Fo&keyword=restaurant&location=16.06673,108.211981&radius=1000
-//        https://developer.android.com/training/volley/simple
-
         restaurants.add(new Restaurant("Nha hang 1",". New York, NY, USA", null));
         restaurants.add(new Restaurant("Nha hang 2",". New York, NY, USA", null));
         restaurants.add(new Restaurant("Nha hang 3",null, null));
